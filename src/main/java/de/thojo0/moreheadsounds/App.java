0927bc5df29d95dd702fb2021733c17a241a0803
thojo0 <53666000+thojo0@users.noreply.github.com>
2024-07-31 22:05:00 +0200

    add clearItem command to force clear the noteblocksound of a player head

--- src/main/java/de/thojo0/moreheadsounds/App.java
+++ src/main/java/de/thojo0/moreheadsounds/App.java
@@ -3,10 +3,16 @@ package de.thojo0.moreheadsounds;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TranslatableComponent;
 
 public class App extends JavaPlugin {
     public String customPrefix = "§a[§e" + getName() + "§a]";
@@ -31,6 +37,10 @@ public class App extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = null;
+        if (sender instanceof Player)
+            player = (Player) sender;
+
         if (args.length == 0) {
             sender.sendMessage(customPrefixFail + " You need to type something after /" + label + "\n");
             return true;
@@ -45,6 +55,33 @@ public class App extends JavaPlugin {
                 eventListener.reloadConfig(getConfig());
                 sender.sendMessage(customPrefix + " Configuration reloaded!");
                 return true;
+
+            case "clearItem":
+                if (player == null) {
+                    sender.sendMessage(customPrefixFail + " You can only this command as player!");
+                    return true;
+                }
+                if (!sender.hasPermission(command.getName() + ".clearItem")) {
+                    sender.sendMessage(customPrefixFail + " You don't have permission to perform that command!");
+                    return true;
+                }
+
+                ItemStack item = player.getInventory().getItemInMainHand();
+
+                // Check if the item is a player head
+                if (item.getType() != Material.PLAYER_HEAD) {
+                    sender.spigot().sendMessage(new ComponentBuilder(customPrefixFail + " You arn't holding a ")
+                            .append(new TranslatableComponent(Material.PLAYER_HEAD.getItemTranslationKey())).build());
+                    return true;
+                }
+                // Get the metadata for the player head
+                SkullMeta metaData = (SkullMeta) item.getItemMeta();
+                // remove the sound from the metadata
+                metaData.setNoteBlockSound(null);
+                item.setItemMeta(metaData);
+
+                sender.sendMessage(customPrefix + " Sound cleared from item!");
+                return true;
         }
         return super.onCommand(sender, command, label, args);
     }
@@ -54,6 +91,9 @@ public class App extends JavaPlugin {
         LinkedList<String> autoComplete = new LinkedList<String>();
         if (args.length == 1 && (sender.hasPermission(command.getName() + ".reload"))) {
             autoComplete.add("reload");
+        }
+        if (args.length == 1 && (sender.hasPermission(command.getName() + ".clearItem"))) {
+            autoComplete.add("clearItem");
         }
         return autoComplete;
     }
