a397297c36631bd0fb351775fd4115773ff3a96d
thojo0 <53666000+thojo0@users.noreply.github.com>
2024-08-05 22:15:08 +0200

    add getHash command fixes #1

--- src/main/java/de/thojo0/moreheadsounds/App.java
+++ src/main/java/de/thojo0/moreheadsounds/App.java
@@ -11,8 +11,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.profile.PlayerProfile;
+
+import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TranslatableComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 
 public class App extends JavaPlugin {
     public String customPrefix = "§a[§e" + getName() + "§a]";
@@ -51,10 +56,8 @@ public class App extends JavaPlugin {
                     sender.sendMessage(customPrefixFail + " You don't have permission to perform that command!");
                     return true;
                 }
-                reloadConfig();
-                eventListener.reloadConfig(getConfig());
-                sender.sendMessage(customPrefix + " Configuration reloaded!");
-                return true;
+                runReload(sender);
+                break;
 
             case "clearItem":
                 if (player == null) {
@@ -65,36 +68,87 @@ public class App extends JavaPlugin {
                     sender.sendMessage(customPrefixFail + " You don't have permission to perform that command!");
                     return true;
                 }
+                runClearItem(player);
+                break;
 
-                ItemStack item = player.getInventory().getItemInMainHand();
-
-                // Check if the item is a player head
-                if (item.getType() != Material.PLAYER_HEAD) {
-                    sender.spigot().sendMessage(new ComponentBuilder(customPrefixFail + " You arn't holding a ")
-                            .append(new TranslatableComponent(Material.PLAYER_HEAD.getItemTranslationKey())).build());
+            case "getHash":
+                if (player == null) {
+                    sender.sendMessage(customPrefixFail + " You can only this command as player!");
                     return true;
                 }
-                // Get the metadata for the player head
-                SkullMeta metaData = (SkullMeta) item.getItemMeta();
-                // remove the sound from the metadata
-                metaData.setNoteBlockSound(null);
-                item.setItemMeta(metaData);
-
-                sender.sendMessage(customPrefix + " Sound cleared from item!");
-                return true;
+                if (!sender.hasPermission(command.getName() + ".getHash")) {
+                    sender.sendMessage(customPrefixFail + " You don't have permission to perform that command!");
+                    return true;
+                }
+                runGetHash(player);
+                break;
         }
-        return super.onCommand(sender, command, label, args);
+        return true;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         LinkedList<String> autoComplete = new LinkedList<String>();
-        if (args.length == 1 && (sender.hasPermission(command.getName() + ".reload"))) {
-            autoComplete.add("reload");
-        }
-        if (args.length == 1 && (sender.hasPermission(command.getName() + ".clearItem"))) {
-            autoComplete.add("clearItem");
+        if (args.length == 1) {
+            if (sender.hasPermission(command.getName() + ".reload")) {
+                autoComplete.add("reload");
+            }
+            if (sender.hasPermission(command.getName() + ".clearItem")) {
+                autoComplete.add("clearItem");
+            }
+            if (sender.hasPermission(command.getName() + ".getHash")) {
+                autoComplete.add("getHash");
+            }
         }
         return autoComplete;
+    }
+
+    public void runReload(CommandSender sender) {
+        reloadConfig();
+        eventListener.reloadConfig(getConfig());
+        sender.sendMessage(customPrefix + " Configuration reloaded!");
+    }
+
+    public void runClearItem(Player player) {
+        ItemStack item = player.getInventory().getItemInMainHand();
+
+        // Check if the item is a player head
+        if (item.getType() != Material.PLAYER_HEAD) {
+            player.spigot().sendMessage(new ComponentBuilder(customPrefixFail + " You arn't holding a ")
+                    .append(new TranslatableComponent(Material.PLAYER_HEAD.getItemTranslationKey())).build());
+            return;
+        }
+        // Get the metadata for the player head
+        SkullMeta metaData = (SkullMeta) item.getItemMeta();
+        // remove the sound from the metadata
+        metaData.setNoteBlockSound(null);
+        item.setItemMeta(metaData);
+
+        player.sendMessage(customPrefix + " Sound cleared from item!");
+    }
+
+    public void runGetHash(Player player) {
+        ItemStack item = player.getInventory().getItemInMainHand();
+
+        // Check if the item is a player head
+        if (item.getType() != Material.PLAYER_HEAD) {
+            player.spigot().sendMessage(new ComponentBuilder(customPrefixFail + " You arn't holding a ")
+                    .append(new TranslatableComponent(Material.PLAYER_HEAD.getItemTranslationKey())).build());
+            return;
+        }
+        // Get the metadata for the player head
+        SkullMeta metaData = (SkullMeta) item.getItemMeta();
+        PlayerProfile owner = metaData.getOwnerProfile();
+        // Check owner exist
+        if (owner == null) {
+            player.sendMessage(customPrefixFail + " Coudn't find any texture hash!");
+            return;
+        }
+
+        String textureHash = EventListener.getTextureHash(owner);
+
+        player.spigot().sendMessage(new ComponentBuilder(customPrefix + " Hash: §r").append(textureHash)
+                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to copy to clipboard")))
+                .event(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, textureHash)).build());
     }
 }
