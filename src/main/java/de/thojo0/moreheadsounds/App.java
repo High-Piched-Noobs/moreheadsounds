f311a578a7ac96f7e52a8369c9a36a66feaa6415
thojo0 <53666000+thojo0@users.noreply.github.com>
2024-08-06 21:38:05 +0200

    add getHash posible block position argument to get from placed block use minecraft internal translations for command errors if posible
    Co-authored-by: PYZR <PYZR@users.noreply.github.com>

--- src/main/java/de/thojo0/moreheadsounds/App.java
+++ src/main/java/de/thojo0/moreheadsounds/App.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Skull;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
@@ -13,6 +15,7 @@ import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.profile.PlayerProfile;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
@@ -20,8 +23,8 @@ import net.md_5.bungee.api.chat.TranslatableComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
 
 public class App extends JavaPlugin {
-    public String customPrefix = "§a[§e" + getName() + "§a]";
-    public String customPrefixFail = "§c[§7" + getName() + "§c]";
+    public String customPrefix = "§a[§e" + getName() + "§a] ";
+    public String customPrefixFail = "§c[§7" + getName() + "§c] ";
 
     private final EventListener eventListener = new EventListener(getConfig());
 
@@ -47,40 +50,75 @@ public class App extends JavaPlugin {
             player = (Player) sender;
 
         if (args.length == 0) {
-            sender.sendMessage(customPrefixFail + " You need to type something after /" + label + "\n");
+            sender.sendMessage(customPrefixFail + "You need to type something after /" + label);
+
             return true;
         }
         switch (args[0]) {
             case "reload":
                 if (!sender.hasPermission(command.getName() + ".reload")) {
-                    sender.sendMessage(customPrefixFail + " You don't have permission to perform that command!");
+                    sender.spigot().sendMessage(new ComponentBuilder(customPrefixFail)
+                            .append(new TranslatableComponent("commands.help.failed")).color(ChatColor.RED)
+                            .build());
                     return true;
                 }
                 runReload(sender);
                 break;
 
             case "clearItem":
-                if (player == null) {
-                    sender.sendMessage(customPrefixFail + " You can only this command as player!");
+                if (!sender.hasPermission(command.getName() + ".clearItem")) {
+                    sender.spigot().sendMessage(new ComponentBuilder(customPrefixFail)
+                            .append(new TranslatableComponent("commands.help.failed")).color(ChatColor.RED)
+                            .build());
                     return true;
                 }
-                if (!sender.hasPermission(command.getName() + ".clearItem")) {
-                    sender.sendMessage(customPrefixFail + " You don't have permission to perform that command!");
+                if (player == null) {
+                    sender.spigot().sendMessage(new ComponentBuilder(customPrefixFail)
+                            .append(new TranslatableComponent("permissions.requires.player")).color(ChatColor.RED)
+                            .build());
                     return true;
                 }
                 runClearItem(player);
                 break;
 
             case "getHash":
-                if (player == null) {
-                    sender.sendMessage(customPrefixFail + " You can only this command as player!");
-                    return true;
-                }
                 if (!sender.hasPermission(command.getName() + ".getHash")) {
-                    sender.sendMessage(customPrefixFail + " You don't have permission to perform that command!");
+                    sender.spigot().sendMessage(new ComponentBuilder(customPrefixFail)
+                            .append(new TranslatableComponent("commands.help.failed")).color(ChatColor.RED)
+                            .build());
                     return true;
                 }
-                runGetHash(player);
+                if (player == null) {
+                    sender.spigot().sendMessage(new ComponentBuilder(customPrefixFail)
+                            .append(new TranslatableComponent("permissions.requires.player")).color(ChatColor.RED)
+                            .build());
+                    return true;
+                }
+                if (args.length == 1) {
+                    runGetHash(player);
+                    break;
+                }
+                if (args.length < 4) {
+                    player.spigot().sendMessage(new ComponentBuilder(customPrefixFail)
+                            .append(new TranslatableComponent("argument.pos3d.incomplete")).color(ChatColor.RED)
+                            .build());
+                    return true;
+                }
+                try {
+                    runGetHash(sender, player.getWorld().getBlockAt(Integer.parseInt(args[1]),
+                            Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+                } catch (NumberFormatException e) {
+                    player.spigot().sendMessage(new ComponentBuilder(customPrefixFail)
+                            .append(new TranslatableComponent("parsing.int.expected")).color(ChatColor.RED)
+                            .build());
+                    return true;
+                }
+                break;
+
+            default:
+                sender.spigot().sendMessage(new ComponentBuilder(customPrefixFail)
+                        .append(new TranslatableComponent("commands.help.failed")).color(ChatColor.RED)
+                        .build());
                 break;
         }
         return true;
@@ -88,6 +126,10 @@ public class App extends JavaPlugin {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        Player player = null;
+        if (sender instanceof Player)
+            player = (Player) sender;
+
         LinkedList<String> autoComplete = new LinkedList<String>();
         if (args.length == 1) {
             if (sender.hasPermission(command.getName() + ".reload")) {
@@ -100,13 +142,34 @@ public class App extends JavaPlugin {
                 autoComplete.add("getHash");
             }
         }
+        if (args.length > 1) {
+            if (args[0].equals("getHash") && player != null && sender.hasPermission(command.getName() + ".getHash")) {
+                Block block = player.getTargetBlockExact(5);
+                if (block != null) {
+                    switch (args.length) {
+                        case 2:
+                            autoComplete.add(block.getX() + "");
+                            autoComplete.add(block.getX() + " " + block.getY());
+                            autoComplete.add(block.getX() + " " + block.getY() + " " + block.getZ());
+                            break;
+                        case 3:
+                            autoComplete.add(block.getY() + "");
+                            autoComplete.add(block.getY() + " " + block.getZ());
+                            break;
+                        case 4:
+                            autoComplete.add(block.getZ() + "");
+                            break;
+                    }
+                }
+            }
+        }
         return autoComplete;
     }
 
     public void runReload(CommandSender sender) {
         reloadConfig();
         eventListener.reloadConfig(getConfig());
-        sender.sendMessage(customPrefix + " Configuration reloaded!");
+        sender.sendMessage(customPrefix + "Configuration reloaded!");
     }
 
     public void runClearItem(Player player) {
@@ -114,7 +177,7 @@ public class App extends JavaPlugin {
 
         // Check if the item is a player head
         if (item.getType() != Material.PLAYER_HEAD) {
-            player.spigot().sendMessage(new ComponentBuilder(customPrefixFail + " You arn't holding a ")
+            player.spigot().sendMessage(new ComponentBuilder(customPrefixFail + "You arn't holding a ")
                     .append(new TranslatableComponent(Material.PLAYER_HEAD.getItemTranslationKey())).build());
             return;
         }
@@ -124,7 +187,7 @@ public class App extends JavaPlugin {
         metaData.setNoteBlockSound(null);
         item.setItemMeta(metaData);
 
-        player.sendMessage(customPrefix + " Sound cleared from item!");
+        player.sendMessage(customPrefix + "Sound cleared from item!");
     }
 
     public void runGetHash(Player player) {
@@ -132,7 +195,7 @@ public class App extends JavaPlugin {
 
         // Check if the item is a player head
         if (item.getType() != Material.PLAYER_HEAD) {
-            player.spigot().sendMessage(new ComponentBuilder(customPrefixFail + " You arn't holding a ")
+            player.spigot().sendMessage(new ComponentBuilder(customPrefixFail + "You arn't holding a ")
                     .append(new TranslatableComponent(Material.PLAYER_HEAD.getItemTranslationKey())).build());
             return;
         }
@@ -141,14 +204,35 @@ public class App extends JavaPlugin {
         PlayerProfile owner = metaData.getOwnerProfile();
         // Check owner exist
         if (owner == null) {
-            player.sendMessage(customPrefixFail + " Coudn't find any texture hash!");
+            player.sendMessage(customPrefixFail + "Coudn't find any texture hash!");
             return;
         }
+        runGetHash(player, owner);
+    }
 
+    public void runGetHash(CommandSender sender, Block block) {
+        // Check if the block above is a player head
+        if (block.getType() != Material.PLAYER_HEAD) {
+            sender.spigot().sendMessage(new ComponentBuilder(customPrefixFail + "Target block isn't a ")
+                    .append(new TranslatableComponent(Material.PLAYER_HEAD.getItemTranslationKey())).build());
+            return;
+        }
+        // Get the skull data and texture hash for the player head
+        Skull skull = (Skull) block.getState();
+        PlayerProfile owner = skull.getOwnerProfile();
+        // Check owner exist
+        if (owner == null) {
+            sender.sendMessage(customPrefixFail + "Coudn't find any texture hash!");
+            return;
+        }
+        runGetHash(sender, owner);
+    }
+
+    public void runGetHash(CommandSender sender, PlayerProfile owner) {
         String textureHash = EventListener.getTextureHash(owner);
-
-        player.spigot().sendMessage(new ComponentBuilder(customPrefix + " Hash: §r").append(textureHash)
-                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to copy to clipboard")))
+        sender.spigot().sendMessage(new ComponentBuilder(customPrefix + "Hash: §r").append(textureHash)
+                .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                        new Text(new TranslatableComponent("chat.copy.click"))))
                 .event(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, textureHash)).build());
     }
 }
